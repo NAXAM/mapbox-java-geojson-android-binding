7b5bbf256cb1308fe08857ceadfbd219eb4084eb
Vu Duc Tuyen <tuyen@naxam.net>
2017-04-16 00:33:44 +0700

    Mapbox Java Geojson 2.0.0

---
+++ Naxam.Mapbox.MapboxJavaGeojson/Additions/GeometryClasses.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Position = global::Mapbox.Services.Commons.Models.Position;
+using PositionList = global::System.Collections.Generic.IList<global::Mapbox.Services.Commons.Models.Position>;
+using PositionListList = global::System.Collections.Generic.IList<global::System.Collections.Generic.IList<global::Mapbox.Services.Commons.Models.Position>>;
+using PositionListListList = global::System.Collections.Generic.IList<global::System.Collections.Generic.IList<global::System.Collections.Generic.IList<global::Mapbox.Services.Commons.Models.Position>>>;
+using JPositionList = global::Android.Runtime.JavaList<global::Mapbox.Services.Commons.Models.Position>;
+using JPositionListList = global::Android.Runtime.JavaList<global::System.Collections.Generic.IList<global::Mapbox.Services.Commons.Models.Position>>;
+using JPositionListListList = global::Android.Runtime.JavaList<global::System.Collections.Generic.IList<global::System.Collections.Generic.IList<global::Mapbox.Services.Commons.Models.Position>>>;
+namespace Mapbox.Services.Commons.Geojson
+{
+	public partial class LineString
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				var native_value = JPositionList.ToLocalJniHandle(Positions);
+
+				return Java.Lang.Object.GetObject<JPositionList>(native_value, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			}
+			set
+			{
+				Positions = (PositionList)value;
+			}
+		}
+	}
+
+	public partial class MultiLineString
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				var native_value = JPositionListList.ToLocalJniHandle(Positions);
+
+				return Java.Lang.Object.GetObject<JPositionListList>(native_value, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			}
+			set
+			{
+				Positions = (PositionListList)value;
+			}
+		}
+	}
+
+	public partial class Point
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				return Positions;
+			}
+			set
+			{
+				Positions = (Position)value;
+			}
+		}
+	}
+
+	public partial class MultiPoint
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				var native_value = JPositionList.ToLocalJniHandle(Positions);
+
+				return Java.Lang.Object.GetObject<JPositionList>(native_value, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			}
+			set
+			{
+				Positions = (PositionList)value;
+			}
+		}
+	}
+
+	public partial class Polygon
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				var native_value = JPositionListList.ToLocalJniHandle(Positions);
+
+				return Java.Lang.Object.GetObject<JPositionListList>(native_value, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			}
+			set
+			{
+				Positions = (PositionListList)value;
+			}
+		}
+	}
+
+	public partial class MultiPolygon
+	{
+		public Java.Lang.Object Coordinates
+		{
+			get
+			{
+				var native_value = JPositionListListList.ToLocalJniHandle(Positions);
+
+				return Java.Lang.Object.GetObject<JPositionListListList>(native_value, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+			}
+			set
+			{
+				Positions = (PositionListListList)value;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Xamarin Mapbox Java Geojson Binding Library
+Xamarin Binding Library for [Mapbox Java Geojson](https://mvnrepository.com/artifact/com.mapbox.mapboxsdk/mapbox-java-geojson)
+
+```
+Install-Package Naxam.Mapbox.MapboxJavaGeojson
+```
